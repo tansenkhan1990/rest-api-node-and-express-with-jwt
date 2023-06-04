35caaaf4507a871b87dac2f58a5a3512c90afb3b
tansenkhan1990 <tansenkhan1990@gmail.com>
2023-06-04 18:39:30 +0200

    add demo user for login functionality

--- rest-api/src/controllers/authController.js
+++ rest-api/src/controllers/authController.js
@@ -1,17 +1,29 @@
 const jwt = require('jsonwebtoken');
 const SECRET_KEY = process.env.SECRET_KEY;
 
+const users = [
+  { id: 1, username: 'user1', password: 'password1' },
+  { id: 2, username: 'user2', password: 'password2' },
+];
+
+
 const authController = {
   login: (req, res) => {
-    // Validate user credentials and generate a JWT
-    // For simplicity, this is just a sample implementation
     const { username, password } = req.body;
-    if (username === 'admin' && password === 'password') {
-      const token = jwtUtils.generateToken({ username });
-      res.json({ token });
-    } else {
-      res.status(401).json({ error: 'Invalid credentials' });
+
+    // Find user by username and password
+    const user = users.find(
+      (user) => user.username === username && user.password === password
+    );
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid username or password' });
     }
+
+    // Generate and sign JWT token
+    const token = jwt.sign({ username: user.username }, SECRET_KEY);
+
+    res.json({ token });
   },
   
   register: (req, res) => {
