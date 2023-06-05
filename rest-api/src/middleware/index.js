7686e32ed230af9d5e520f9c7d317a100dd08a44
tansenkhan1990 <tansenkhan1990@gmail.com>
2023-06-05 18:46:03 +0200

    new

--- rest-api/src/middleware/index.js
+++ rest-api/src/middleware/index.js
@@ -1,17 +1,27 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 const SECRET_KEY = process.env.SECRET_KEY;
-const blacklist = require("../utils/blacklist");
+const blacklist = require('../utils/blacklist');
 
 // Middleware to verify the JWT token
 const verifyToken = (req, res, next) => {
   const token = req.headers.authorization;
 
-  // Check if the token is already in the blacklist
-  if (blacklist.includes(token)) {
-    return res.status(401).json({ message: "unauthorize request" });
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided' });
   }
-  res.json({message:'request successfull for protected route'})
-  next();
+
+  const tokenValue = token.split(' ')[1]; // Assuming the token is sent as 'Bearer <token>'
+
+  jwt.verify(tokenValue, SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ error: 'Failed to authenticate token' });
+    }
+
+    req.user = decoded;
+    next();
+  });
 };
+
+
 
 module.exports = verifyToken;
